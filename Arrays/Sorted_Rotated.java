6c78282e1b894b262a2425d6315a49a2fbea534c
ISHAN KASHYAP <148844804+Ishan7k@users.noreply.github.com>
2026-05-09 19:46:29 +0530

    Add solution to check rotated sorted array
    Implement a solution to check if an array was originally sorted and then rotated.

---
+++ Arrays/Sorted_Rotated.java
@@ -0,0 +1,18 @@
+// 1752- Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+// There may be duplicates in the original array.
+// Note: An array A rotated by x positions results in an array B of the same length such that B[i] == A[(i+x) % A.length] for every valid index i.
+
+class Solution {
+    public boolean check(int[] nums) {
+        int count =0;
+
+        for(int i=0;i<nums.length;i++)
+        {
+            int next= nums[(i+1)%nums.length]; //(4+1)%5 = 0 → next = nums[0] = 3 ✅ (wrapped back)
+
+            if(nums[i]> next) count++;
+
+        }
+        return count<=1;
+    }
+}
