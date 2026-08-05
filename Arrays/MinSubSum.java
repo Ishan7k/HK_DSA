b191155267a0b7fef367ddb7e599cdc60929e784
ISHAN KASHYAP <148844804+Ishan7k@users.noreply.github.com>
2026-08-05 16:01:06 +0530

    Easiest Sliding Window Soln with Explanation
    Intuition:-
    Hire & Fire...
    
    Approach:-
    if condition of whose sum is greater than or equal to target is satisfied, then shrink the window by removing the elements until satisfied...hence gives us the min len of subarray required.

---
+++ Arrays/MinSubSum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int low=0;
+        int high=0;
+        int sum=0;
+        int res= Integer.MAX_VALUE;
+
+        while(high<nums.length)
+        { //Expand window
+            sum=sum+nums[high];
+
+            while(sum>=target)
+            { //Shrink window for shortest len
+                int len= high-low+1;
+                res= Math.min(len,res); //shortest len stored
+                sum=sum-nums[low]; // shrinking window- removed 1st element
+                low++;
+            }
+            high++;
+        }
+        if(res==Integer.MAX_VALUE) //target ya usse bda mila hi nhi
+        {
+            return 0;
+        }
+        else
+        {
+            return res;
+        }
+    }
+}
